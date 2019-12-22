736b35f3836e4256e6ec3faf4b45e87bd3402e5a
marybecker <mary.becker@ct.gov>
2019-12-22 16:08:02 -0500

    slight style update and code cleanup

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # Predicted Stream Health in Connecticut
-Map and model to predict stream health based on the CT macroinvertebrate multimetric index (MMI)
-
-https://marybecker.github.io/CT_MMI_StreamModel/
 
 Classifying the Health of Connecticut Streams Using Benthic Macroinvertebrates with Implications for Water Management
 Christopher J. Bellucci • Mary E. Becker • Mike Beauchene • Lee Dunbar
-Environmental Management, 2013 DOI 10.1007/s00267-013-0033-9
+Environmental Management, 2013 [DOI 10.1007/s00267-013-0033-9](https://doi.org/10.1007/s00267-013-0033-9)
+
+Map displaying model predictions of stream health in Connecticut:
+
+https://marybecker.github.io/CT_MMI_StreamModel/
+
+Model development and data used for predictions:
+
+https://github.com/marybecker/CT_MMI_StreamModel
 
 ## Abstract 
 
@@ -21,9 +26,9 @@ landscape condition and watershed characteristics and
 when combined with measurements of stream biology,
 provide a useful visual display of information that is useful
 in a management context. The objective of our study was to
-estimate the stream health for all wadeable stream kilo-
-meters in Connecticut using a combination of macroin-
-vertebrate metrics and landscape variables. We developed
+estimate the stream health for all wadeable stream kilometers 
+in Connecticut using a combination of macroinvertebrate 
+metrics and landscape variables. We developed
 and evaluated models using an information theoretic
 approach to predict stream health as measured by macro-
 invertebrate multimetric index (MMI) and identified the
@@ -34,3 +39,4 @@ R 2 = 0.56, SE = 11.73). We then provide examples of
 how modeling can augment existing programs to support
 water management policies under the Federal Clean Water
 Act such as stream assessments and anti-degradation.
+
--- main.js
+++ main.js
@@ -35,93 +35,13 @@ var breaks =    [[75, 'High Quality','(>75 MMI)','#0868ac'],
                 [43,'Moderate Low Quality','(43 - 60 MMI)','#7bccc4'],
                 [20,'Low Quality','(<43 MMI)','#bae4bc']];
 
-function getColor(d) {
-    if(d >= breaks[0][0]) {
-        return '#0868ac';
-    } else if(d >= breaks[1][0]) {
-        return '#43a2ca';
-    } else if(d >= breaks[2][0]) {
-        return '#7bccc4';
-    } else {
-        return '#bae4bc'
-    }
-}
-
-function getQual(m){
-    if(m >= breaks[0][0]) {
-        return breaks[0][1];
-    } else if(m >= breaks[1][0]) {
-        return breaks[1][1];
-    } else if(m >= breaks[2][0]) {
-        return breaks[2][1];
-    } else {
-        return breaks[3][1]
-    }
-}
-
-function style(feature) {
-    return {
-        fillColor: getColor(feature.properties.MODMMI),
-        weight: 2,
-        opacity: 0,
-        color: 'white',
-        fillOpacity: 0.6
-    };
-}
-
 var customPopup =
     {
         // 'maxWidth': '500 px',
         'className' : 'customOptions'
     };
 
-function onEachFeature(feature, layer) {
-    var props = layer.feature.properties;
-    if (props['GNIS_NAME']!= null){
-        streamName= props['GNIS_NAME']
-    }else{
-        streamName= 'No Name Stream'
-    }
-    var popup = `<b>Stream Name:</b> ${streamName} <br> Prediced MMI: ${Math.round(props['MODMMI'])} - ${getQual(props['MODMMI'])}`;
-    if (L.Browser.mobile) {
-        layer.bindPopup(popup,customPopup);
-    } else {
-        layer.bindTooltip(popup,{
-            className: 'customTooltip'
-        });
-    };
-}
-
-function filter (feature) {
-    if (feature.properties['MODMMI'] != null){
-        return feature;
-    }
-}
-
-function drawLegend(breaks) {
-    var legend = L.control({position: 'topleft'});
-    legend.onAdd = function () {
-        var div = L.DomUtil.create('div', 'legend');
-        div.innerHTML = "<h3>" +'Predicted Stream Health in CT'+ "</h3>";
-        for (var i = 0; i < breaks.length; i++) {
-            var color = getColor(breaks[i][0], breaks);
-            div.innerHTML +=
-                '<span style="background:' + color + '"></span> ' +
-                '<label>'+(breaks[i][1]).toLocaleString()+" "+(breaks[i][2]).toLocaleString()+ '</label>';
-        }
-
-        return div;
-    };
-    legend.addTo(map);
-}
-
-//Only draw legend if view screen is large enough
-if (screen.width> 1024) {
-    drawLegend(breaks);
-    $("h1").remove()
-}
-
-//Load layers
+/* --------------- Load map layers ---------------  */
 var ctBoundJSON = $.getJSON("data/CT_state_boundary.geojson",function(data){
     var CTBound = L.geoJSON(data,{
         style: function (feature) {
@@ -180,6 +100,90 @@ var LowJSON = $.when(modLowJSON).done(function () {$.getJSON("data/MMIMod_LowQua
 
 });
 
+//Get colors for map
+function getColor(d) {
+    if(d >= breaks[0][0]) {
+        return '#0868ac';
+    } else if(d >= breaks[1][0]) {
+        return '#43a2ca';
+    } else if(d >= breaks[2][0]) {
+        return '#7bccc4';
+    } else {
+        return '#bae4bc'
+    }
+}
+
+//Get breaks for stream quality
+function getQual(m){
+    if(m >= breaks[0][0]) {
+        return breaks[0][1];
+    } else if(m >= breaks[1][0]) {
+        return breaks[1][1];
+    } else if(m >= breaks[2][0]) {
+        return breaks[2][1];
+    } else {
+        return breaks[3][1]
+    }
+}
+
+//Set style for all stream catchment layers
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.MODMMI),
+        weight: 2,
+        opacity: 0,
+        color: 'white',
+        fillOpacity: 0.6
+    };
+}
+
+//Set popups for all stream catchment layers
+function onEachFeature(feature, layer) {
+    var props = layer.feature.properties;
+    if (props['GNIS_NAME']!= null){
+        streamName= props['GNIS_NAME']
+    }else{
+        streamName= 'No Name Stream'
+    }
+    var popup = `<b>Stream Name:</b> ${streamName} <br> Prediced MMI: ${Math.round(props['MODMMI'])} - ${getQual(props['MODMMI'])}`;
+    if (L.Browser.mobile) {
+        layer.bindPopup(popup,customPopup);
+    } else {
+        layer.bindTooltip(popup,{
+            className: 'customTooltip'
+        });
+    };
+}
+
+//Only display catchments with data
+function filter (feature) {
+    if (feature.properties['MODMMI'] != null){
+        return feature;
+    }
+}
+
+//Draw legend with stream quality breaks
+function drawLegend(breaks) {
+    var legend = L.control({position: 'topleft'});
+    legend.onAdd = function () {
+        var div = L.DomUtil.create('div', 'legend');
+        div.innerHTML = "<h3>" +'Predicted Stream Health in CT'+ "</h3>";
+        for (var i = 0; i < breaks.length; i++) {
+            var color = getColor(breaks[i][0], breaks);
+            div.innerHTML +=
+                '<span style="background:' + color + '"></span> ' +
+                '<label>'+(breaks[i][1]).toLocaleString()+" "+(breaks[i][2]).toLocaleString()+ '</label>';
+        }
+
+        return div;
+    };
+    legend.addTo(map);
+}
+
+//Only draw legend if view screen is large enough
+if (screen.width>= 1024) {
+    drawLegend(breaks);
+}
 
 /* --------------- Toggle on/off info footer content ---------------  */
 var clicked = false;
